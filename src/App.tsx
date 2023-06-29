58631694fb9ba0c5b8dd8dfe8cd030c8bc7f7504
lifeike <lifeike@gmail.com>
2023-06-28 22:28:44 -0600

    ad

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useRef, useEffect, memo } from "react"
-import AbcIcon from "@mui/icons-material/Abc"
+import HomeLayout from "./components/HomeLayout"
 
 const App = (props: {}) => {
   return (
-    <>
+    <HomeLayout>
       <h2 className="text-red-800">App</h2>
-    </>
+    </HomeLayout>
   )
 }
 
---
+++ src/components/HomeLayout/index.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useRef, useEffect, memo } from "react"
+import AppBar from "./AppBar"
+
+const HomeLayout = (props: {}) => {
+  return (
+    <>
+      <AppBar />
+      <h2>HomeLayout</h2>
+    </>
+  )
+}
+
+export default HomeLayout
