cb8f043cc6ecd66ac4a287bb42f764831d988a78
lifeike <lifeike@gmail.com>
2023-06-28 22:22:32 -0600

    ad

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, { useState, useRef, useEffect, memo } from "react"
+import AbcIcon from "@mui/icons-material/Abc"
 
 const App = (props: {}) => {
   return (
     <>
-      <h2>App</h2>
+      <h2 className="text-red-800">App</h2>
     </>
   )
 }
