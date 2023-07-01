48af9c4ed02cdee6e7f0f37870dec456fa26144e
lifeike <lifeike@gmail.com>
2023-07-01 14:49:11 -0600

    ad

---
+++ src/utils/session.tsx
@@ -0,0 +1,24 @@
+export const getSession = () => {
+  let sessionJson = localStorage.getItem("session")
+  if (sessionJson) {
+    try {
+      return JSON.parse(sessionJson)
+    } catch (err) {
+      return
+    }
+  }
+  return
+}
+
+export const setSession = (session: { accessToken: string; refreshToken: string }) => {
+  localStorage.setItem("session", JSON.stringify(session))
+}
+
+export const clearSession = () => {
+  localStorage.removeItem("session")
+}
+
+export const isAuthenticated = () => {
+  const session = getSession()
+  return session != "" && session !== undefined && session.access_token !== undefined && session.refresh_token !== undefined
+}
