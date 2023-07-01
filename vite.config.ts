2ee3e4396a84072296c2d79af1c5c2480839301c
lifeike <lifeike@gmail.com>
2023-07-01 12:55:00 -0600

    ad

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { fileURLToPath, URL } from "url"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 },
+  server: { port: 5173 },
 })
