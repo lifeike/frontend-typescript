df56de2568623fb844b39d467c1bfca98b6d0fac
lifeike <lifeike@gmail.com>
2023-07-02 11:59:21 -0600

    ad

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import "./index.css"
 
+//clear chrome dev console on every source code change or HMR
+if (import.meta.hot) import.meta.hot.on("vite:beforeUpdate", () => console.clear())
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
