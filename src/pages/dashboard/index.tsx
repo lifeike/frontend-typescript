4b47877d3b0783edb9b1eed5c46e1743e8636a3f
lifeike <lifeike@gmail.com>
2023-07-02 18:15:49 -0600

    ad

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useRef, useEffect, memo } from "react"
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  )
+}
+
+export default Dashboard
