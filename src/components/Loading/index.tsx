29aca582ec9ed49ba80f19aa582d5df93b41061f
lifeike <lifeike@gmail.com>
2023-07-03 12:51:21 -0600

    ad

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -6,22 +6,13 @@ import Button from "@mui/material/Button"
 
 export default function SimpleBackdrop() {
   const [open, setOpen] = React.useState(false)
-  const handleClose = () => {
-    setOpen(false)
-  }
-  const handleOpen = () => {
-    setOpen(true)
-  }
 
   // The `state` arg is correctly typed as `RootState` already
-  const test = useAppSelector((state) => state.loading)
-  console.log(test)
-  const dispatch = useAppDispatch()
+  const loading = useAppSelector((state) => state.loading.value)
 
   return (
     <div>
-      <Button onClick={handleOpen}>Show backdrop</Button>
-      <Backdrop sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }} open={open} onClick={handleClose}>
+      <Backdrop sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }} open={open} onClick={() => setOpen(loading)}>
         <CircularProgress color="inherit" />
       </Backdrop>
     </div>
