c72e10b288ba4044b6b2994d66da1bd0bc085874
lifeike <lifeike@gmail.com>
2023-07-03 12:31:55 -0600

    ad

--- src/store/actions/movie.ts
+++ src/store/actions/movie.ts
@@ -3,6 +3,9 @@ import * as api from "@/api/movie"
 
 // First, create the thunk
 export const getMovies = createAsyncThunk("users/fetchByIdStatus", async (data, thunkAPI) => {
+  console.log("test")
+  console.log(thunkAPI)
+  console.log("test")
   const response = await api.getMovie(data)
   return response
 })
