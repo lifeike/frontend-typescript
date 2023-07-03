82daece72184a7a695088e279ed4ab8a72c39271
lifeike <lifeike@gmail.com>
2023-07-03 13:11:21 -0600

    ad

--- src/store/actions/movie.ts
+++ src/store/actions/movie.ts
@@ -3,10 +3,9 @@ import * as api from "@/api/movie"
 import * as loading from "../reducers/loading"
 
 // First, create the thunk
-export const getMovies = createAsyncThunk("users/fetchByIdStatus", async (data, thunkAPI) => {
+export const getMovies = createAsyncThunk("", async (data, thunkAPI) => {
   thunkAPI.dispatch(loading.turnOn())
   const response = await api.getMovie(data)
-  console.log(response)
   thunkAPI.dispatch(loading.turnOff())
   return response
 })
