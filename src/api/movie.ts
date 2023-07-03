4208e48ccbb781a25b69ab05a795466474e0afc5
lifeike <lifeike@gmail.com>
2023-07-03 00:57:29 -0600

    ad

--- src/api/movie.ts
+++ src/api/movie.ts
@@ -1,3 +1,32 @@
-const api = {}
+import axios from "@/utils/service"
 
-export default api
+export function getMovie(data: any) {
+  return axios({
+    url: `/movie/getMovies`,
+    method: "get",
+    params: data,
+  })
+}
+
+export function findMovie(data: any) {
+  return axios({
+    url: `/movie/findOneMovie`,
+    method: "post",
+    data,
+  })
+}
+
+export function updateMovie(data: any) {
+  return axios({
+    url: `/movie/updateMovie/${data.id}`,
+    method: "post",
+    data: data.object,
+  })
+}
+
+export function deleteMovie(data: any) {
+  return axios({
+    url: `/movie/deleteMovie/${data.id}`,
+    method: "delete",
+  })
+}
