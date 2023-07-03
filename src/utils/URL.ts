24809b6e9535c667ff1176c90e25bdf63e3fcdd5
lifeike <lifeike@gmail.com>
2023-07-03 01:10:00 -0600

    ad

--- src/utils/URL.ts
+++ src/utils/URL.ts
@@ -1,5 +1,6 @@
 //backend api
-export const baseURL = import.meta.env.MODE == "development" ? "https://api.apis.guru/v2/list.json" : "https://nap5l3bq7e.execute-api.us-east-1.amazonaws.com/"
+export const baseURL =
+  import.meta.env.MODE == "development" ? "https://date.nager.at/api/v2/publicholidays/2020/US" : "https://nap5l3bq7e.execute-api.us-east-1.amazonaws.com/"
 
 //configure setupProxy.js
 //other api 1
