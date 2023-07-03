f8bed85d675707f97dff4e842cecb77e4dc0439a
lifeike <lifeike@gmail.com>
2023-07-02 21:27:35 -0600

    ad

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,9 +2,7 @@ import { configureStore } from "@reduxjs/toolkit"
 import counterReducer from "./reducers/counter"
 
 const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: { counter: counterReducer },
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
